633365753077f011ffb467f58ced09606f578b02
samuelduque <duquepuchi@gmail.com>
2022-04-17 20:26:41 +0200

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '92b87286581461433e20242ffb312bed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c32d0cd5-dda1-4900-b7f8-326aa4aa79ee'
+};
